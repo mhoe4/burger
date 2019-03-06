211282f91052161b457c8d64462a2e711a025d8c
mhoef <mhoefer1@gmail.com>
2019-03-05 19:22:11 -0500

    work

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE burgers_db;
+
+
+INSERT INTO burgers (burger_name) VALUES ('Bacon Burger');
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Plain Burger');
