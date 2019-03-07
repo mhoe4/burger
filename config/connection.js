5d1baa0df744614a76151c0dc9b0e6c3e88a73dd
mhoef <mhoefer1@gmail.com>
2019-03-07 13:09:53 -0500

    lol

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,30 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "root",
+//   database: "burgers_db"
+// });
 
+
+// var connection = mysql.createConnection({
+//   port: 1433,
+//   host: 'tcp:gtbootcamp.database.windows.net',
+//   user: 'mark@gtbootcamp.database.windows.net',
+//   password: "Huskers99",
+//   database: "gtbootcamp" 
+// });
+
+var connection = mysql.createConnection({
+  port: 3306,
+  host: 'h2cwrn74535xdazj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+  user: 'juj0mn01g5cse60f',
+  password: "xy2ki3dmnjdip7yk",
+  database: "ticikt0vkm7hco7g" 
+});
 
 // Make connection.
 connection.connect(function(err) {
